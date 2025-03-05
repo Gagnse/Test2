abd9c9bf9ca5c7b0205383c7d352c48b3ccc6342
Gagnse <sebastien.gagnon.23@ulaval.ca>
2025-03-05 08:59:06 -0500

    1er

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__,
             static_folder='frontend/static',
@@ -10,15 +10,23 @@ def home():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')  # à créer
+    return render_template('about.html')
 
 @app.route('/services')
 def services():
-    return render_template('services.html')  # à créer
+    return render_template('services.html') 
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')  # à créer
+    return render_template('contact.html')
+
+@app.route('/privacy')
+def privacy():
+    return render_template('privacy_policy.html')
+
+@app.route('/terms')
+def terms():
+    return render_template('terms_of_service.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
